5014047c1b957bc71455de5b3a1c15c47951d22f
matheus.falavigna <matheusfalavigna@gmail.com>
2023-09-03 19:00:31 -0300

    feat: criação do Header

---
+++ app/components/Header/Header.style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--yellow);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  max-height: 4rem;
+  margin: 0 auto;
+
+  padding: 1rem 0.5rem;
+
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  img {
+    width: 100%;
+    max-width: 3rem;
+    border-radius: 50%;
+  }
+`;
