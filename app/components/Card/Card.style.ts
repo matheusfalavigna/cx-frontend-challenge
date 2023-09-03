9e8008a225c921b37a5c86a05c9295cf61a080ac
matheus.falavigna <matheusfalavigna@gmail.com>
2023-09-03 19:00:56 -0300

    feat: criação do Card

---
+++ app/components/Card/Card.style.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  padding-bottom: 1rem;
+
+  li {
+    width: 100%;
+    list-style: none;
+    max-width: 70rem;
+    background: var(--background-card);
+
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      width: 100%;
+      padding: 1rem 0 0 1rem;
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      gap: 1rem;
+      color: var(--text);
+
+      h2 {
+        font-size: 1.5rem;
+        font-weight: 700;
+      }
+
+      strong {
+        font-size: 1.3rem;
+        font-weight: 400;
+      }
+
+      span {
+        color: var(--text-installments);
+      }
+
+      .priceAndShipping {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1rem;
+      }
+    }
+
+    .img {
+      width: 17%;
+      border-radius: 0.5rem;
+    }
+
+    .address {
+      max-width: 20rem;
+      flex-direction: row-reverse;
+      margin: 2.5rem 6rem 0 0;
+      color: var(--text-adress);
+
+      p {
+        font-size: 0.9rem;
+      }
+    }
+  }
+`;
