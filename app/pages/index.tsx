58d5b8b9cc8fa1f638e5bda8b39d649fa750b417
matheus.falavigna <matheusfalavigna@gmail.com>
2023-09-04 23:43:48 -0300

    feat: adicionando novas funções para o filtro

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductProvider, useProductContext } from "../hook/ProductContext";
 import { searchProducts } from "@/services/products";
 import { Header } from "@/components/Header/Header";
 import { Dashboard } from "@/components/Dashboard/Dashboard";
 
 function HomePage() {
-  const { setProducts } = useProductContext();
+  const { setProducts, setSelectedSort } = useProductContext();
+  const [searchText, setSearchText] = useState("");
+  const [sortOption, setSortOption] = useState("relevance");
+
   async function heandleSearch(searchText: string) {
-    setProducts(await searchProducts(searchText));
+    setSearchText(searchText);
+    setProducts(await searchProducts(searchText, sortOption));
+  }
+
+  function handleSortChange(newSortOption: string) {
+    setSortOption(newSortOption);
+    setSelectedSort(newSortOption);
   }
 
   return (
     <>
       <Header onSearch={heandleSearch} />
-      <Dashboard />
+      <Dashboard
+        searchText={searchText}
+        sortOption={sortOption}
+        onSortChange={handleSortChange}
+      />
     </>
   );
 }
