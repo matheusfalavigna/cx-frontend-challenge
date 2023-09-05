261feb70b60560299ae9c0c9783c8c2559a8b4eb
matheus.falavigna <matheusfalavigna@gmail.com>
2023-09-04 23:41:14 -0300

    feat: adicionando o componente Sort

--- app/components/Dashboard/Dashboard.tsx
+++ app/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "./Dashboard.style";
 import { Card } from "../Card/Card";
 import { useProductContext } from "@/hook/ProductContext";
+import { Sort } from "../Sort/Sort";
+import { searchProducts } from "@/services/products";
 
-const ProductList = () => {
-  const { products } = useProductContext();
+interface DashboardProps {
+  searchText: string;
+  sortOption: string;
+  onSortChange: (newSortOption: string) => void;
+}
+
+interface ProductListProps {
+  searchText: string;
+  sortOption: string;
+}
+
+const ProductList = ({ searchText, sortOption }: ProductListProps) => {
+  const { products, setProducts } = useProductContext();
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const sortedProducts = await searchProducts(searchText, sortOption);
+        setProducts(sortedProducts);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      }
+    }
+
+    fetchProducts();
+  }, [searchText, sortOption]);
 
   return (
     <ul>
@@ -20,10 +46,34 @@ const ProductList = () => {
   );
 };
 
-export function Dashboard() {
+export function Dashboard({
+  searchText,
+  sortOption,
+  onSortChange,
+}: DashboardProps) {
+  const sortOptions = [
+    {
+      id: "relevance",
+      name: "Mais relevantes",
+    },
+    {
+      id: "price_desc",
+      name: "Maior preço",
+    },
+    {
+      id: "price_asc",
+      name: "Menor preço",
+    },
+  ];
+
   return (
     <Container>
-      <ProductList />
+      <Sort
+        sortOptions={sortOptions}
+        selectedSort={sortOption}
+        onSortChange={onSortChange}
+      />
+      <ProductList searchText={searchText} sortOption={sortOption} />
     </Container>
   );
 }
