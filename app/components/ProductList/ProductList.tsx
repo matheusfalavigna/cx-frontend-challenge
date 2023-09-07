e6614d32051986589fb9915d94a13b5146f82198
matheus.falavigna <matheusfalavigna@gmail.com>
2023-09-07 10:13:28 -0300

    feat: atulizando estados para Redux

--- app/components/ProductList/ProductList.tsx
+++ app/components/ProductList/ProductList.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { useProductContext } from "@/hook/ProductContext";
+// import { useProductContext } from "@/hook/ProductContext";
 import { Card } from "../Card/Card";
 import { Container } from "./ProductList.style";
+import { useSelector } from "react-redux";
+import { Product } from "@/types/types";
+
+interface RootStateWithProduct extends Product {
+  products: {
+    products: Product[];
+  };
+}
 
 export function ProductList() {
-  const { products } = useProductContext();
+  const { products } = useSelector(
+    (state: RootStateWithProduct) => state.products
+  );
 
   return (
     <Container>
